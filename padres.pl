329ab27c9d329e607be97464798f1bf5d2867717
Uriel Rubio <rubio_esc_uriel@hotmail.com>
2018-11-20 22:37:37 -0600

    declaracion solo de hechos

---
+++ padres.pl
@@ -0,0 +1,14 @@
+espadre(carlos, arturo).
+esmadre(gema, arturo).
+espadre(luis, juana).
+esmadre(susan, juana).
+espadre(arturo, juan).
+espadre(arturo, maria).
+espadre(arturo, pedro).
+espadre(juana, juan).
+espadre(juana, maria).
+espadre(juana, pedro).
+
+%esabuelo()
+%eshermano()
+%eshijo()
